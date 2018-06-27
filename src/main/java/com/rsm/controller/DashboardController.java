39793e4b3f6d036cea623b449a050efd9204cf02
Dawid Świnoga <dawid.swinoga@gmail.com>
2018-06-27 18:38:13 +0200

    Add customer reports sorting.

--- src/main/java/com/rsm/controller/DashboardController.java
+++ src/main/java/com/rsm/controller/DashboardController.java
@@ -2,21 +2,27 @@ package com.rsm.controller;
 
 import com.rsm.customer.Customer;
 import com.rsm.customer.CustomerService;
-import com.rsm.device.Device;
 import com.rsm.device.DeviceService;
 import com.rsm.employee.Employee;
 import com.rsm.employee.EmployeeService;
-import com.rsm.report.*;
+import com.rsm.report.Report;
+import com.rsm.report.ReportCopy;
+import com.rsm.report.ReportCopyService;
+import com.rsm.report.ReportDoesNotExistException;
+import com.rsm.report.ReportService;
 import com.rsm.role.RoleService;
 import com.rsm.user.User;
 import com.rsm.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -25,9 +31,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.Principal;
 import java.time.LocalDate;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Optional.ofNullable;
 
 
 @Controller
@@ -44,16 +54,16 @@ public class DashboardController {
     private static final String masterRole = "MASTER";
     private static final String employeeRole = "EMPLOYEE";
     private static final String customerRole = "CUSTOMER";
-    private static final String UPLOADED_FOLDER="src/main/resources/static/images/uploads/";
+    private static final String UPLOADED_FOLDER = "src/main/resources/static/images/uploads/";
 
     @GetMapping("")
-    public String dashboard(Principal principal){
+    public String dashboard(Principal principal) {
         Optional<User> optionalUser = userService.findByUsername(principal.getName());
-        if(optionalUser.isPresent()) {
+        if (optionalUser.isPresent()) {
             User user = optionalUser.get();
-            if(user.getRoles().contains(roleService.findRoleByRoleName(masterRole).orElse(null))) {
+            if (user.getRoles().contains(roleService.findRoleByRoleName(masterRole).orElse(null))) {
                 return "redirect:/dashboard/masterDashboard/";
-            } else if(user.getRoles().contains(roleService.findRoleByRoleName(employeeRole).orElse(null))) {
+            } else if (user.getRoles().contains(roleService.findRoleByRoleName(employeeRole).orElse(null))) {
                 return "redirect:/dashboard/employeeDashboard/";
             } else if (user.getRoles().contains(roleService.findRoleByRoleName(customerRole).orElse(null))) {
                 return "redirect:/dashboard/customerDashboard/";
@@ -79,15 +89,18 @@ public class DashboardController {
     }
 
     @GetMapping("/customerDashboard")
-    public String customerDashboard(Model model,Principal principal) {
-        String username=principal.getName();
-        Optional<Customer> optionalCustomer=customerService.findByUsername(username);
-        if(optionalCustomer.isPresent()){
-            List<Report> reports=optionalCustomer.get().getReports();
+    public String customerDashboard(Model model, Principal principal) {
+        String username = principal.getName();
+        Optional<Customer> optionalCustomer = customerService.findByUsername(username);
+        if (optionalCustomer.isPresent()) {
+            List<Report> reports = optionalCustomer.get().getReports();
+            reports = ofNullable(reports).map(that -> that.stream()
+                    .sorted(Comparator.comparing(Report::getReportDate).reversed())
+                    .collect(Collectors.toList())).orElse(new ArrayList<>());
             int employeeCounter = employeeService.findAll().size();
             int deviceCounter = deviceService.findAll().size();
-            model.addAttribute("reports",reports);
-            model.addAttribute("reportCounter",reports.size());
+            model.addAttribute("reports", reports);
+            model.addAttribute("reportCounter", reports.size());
             model.addAttribute("deviceCounter", deviceCounter);
             model.addAttribute("employeeCounter", employeeCounter);
         }
@@ -97,10 +110,10 @@ public class DashboardController {
     @GetMapping("/customerDashboard/{reportId}/details")
     public String customerReportDetails(@PathVariable Long reportId, Model model, Principal principal) {
         Report report = reportService.findById(reportId).orElseThrow(ReportDoesNotExistException::new);
-        String username=principal.getName();
-        Optional<Customer> optionalCustomer=customerService.findByUsername(username);
-        if(optionalCustomer.isPresent()) {
-            if(optionalCustomer.get().getReports().contains(report)) {
+        String username = principal.getName();
+        Optional<Customer> optionalCustomer = customerService.findByUsername(username);
+        if (optionalCustomer.isPresent()) {
+            if (optionalCustomer.get().getReports().contains(report)) {
                 model.addAttribute("report", report);
                 model.addAttribute("reportCopies", report.getReportCopies());
             }
@@ -135,9 +148,9 @@ public class DashboardController {
     }
 
     @GetMapping("/customerDashboard/{reportId}/details/edit")
-    public String editReport(@PathVariable Long reportId, Model model){
-        Optional<Report> optionalReport=reportService.findById(reportId);
-        if(optionalReport.isPresent()) {
+    public String editReport(@PathVariable Long reportId, Model model) {
+        Optional<Report> optionalReport = reportService.findById(reportId);
+        if (optionalReport.isPresent()) {
             ReportCopy reportCopy = new ReportCopy();
             reportCopy.setDescription(optionalReport.get().getDescription());
             model.addAttribute("report", reportCopy);
@@ -146,13 +159,13 @@ public class DashboardController {
     }
 
     @PostMapping("/customerDashboard/{reportId}/details/edit")
-    public String editReportAddReportCopy(@PathVariable Long reportId, @ModelAttribute("report")ReportCopy reportCopy, BindingResult result, Model model,
-                                          @RequestParam(name = "file",required = false) MultipartFile file,
-                                          Principal principal){
-        Optional<Report> optionalReport=reportService.findById(reportId);
-        if(optionalReport.isPresent()) {
+    public String editReportAddReportCopy(@PathVariable Long reportId, @ModelAttribute("report") ReportCopy reportCopy, BindingResult result, Model model,
+                                          @RequestParam(name = "file", required = false) MultipartFile file,
+                                          Principal principal) {
+        Optional<Report> optionalReport = reportService.findById(reportId);
+        if (optionalReport.isPresent()) {
             Report originalReport = optionalReport.get();
-            if(file!=null && !file.isEmpty()) {
+            if (file != null && !file.isEmpty()) {
                 try {
                     byte[] bytes = file.getBytes();
                     reportCopy.setReportPhoto(bytes);
